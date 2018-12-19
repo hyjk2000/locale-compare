1362950f7441d365adb25d55bac4eac908b6a136
James Shih <hyjk2000@users.noreply.github.com>
2018-12-19 11:23:17 +0800

    fix: check Intl locale support

--- src/locale-compare.js
+++ src/locale-compare.js
@@ -1,7 +1,15 @@
 "use strict";
 
+function detectCollator(locale) {
+	return (
+		Intl
+		&& "Collator" in Intl
+		&& (typeof locale === "undefined" || Intl.Collator.supportedLocalesOf(locale).length > 0)
+	);
+}
+
 function localeCompare(locale) {
-  return Intl && "Collator" in Intl
+  return detectCollator(locale)
     ? new Intl.Collator(locale).compare
     : function(a, b) {
         return a.localeCompare(b);
--- test/locale-compare-test.js
+++ test/locale-compare-test.js
@@ -2,10 +2,18 @@
 
 const assert = require("assert");
 
-const compare = require("../src/locale-compare")();
+const compare = require("../src/locale-compare");
 
 (function test_works() {
-	assert.ok(compare("ä", "z") < 0);
+	assert.ok(compare()("ä", "z") < 0);
 })();
+
+if (Intl
+		&& "Collator" in Intl
+		&& Intl.Collator.supportedLocalesOf("sv").length > 0) {
+	(function test_works_with_specified_locale() {
+		assert.ok(compare("sv")("ä", "z") > 0);
+	})();
+}
 
 console.log("All tests passed.");
